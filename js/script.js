edb07493449306eb61c48f46b70721a3f09fb743
thenaijagamer <sabdullahialaba50@gmail.com>
2024-01-26 00:32:51 +0100

    Add the update launch time functionality

--- js/script.js
+++ js/script.js
@@ -1,35 +1,58 @@
-// const
-let time = 86400 * 15; // seconds
+// Initial time declaration
+let inTime = (+new Date(2024, 0, 26, 0, 5, 0) - new Date().getTime()) / 1000; // time stamp in seconds
+
+let time =
+  inTime <= 0
+    ? (+new Date(localStorage.date) - new Date().getTime()) / 1000
+    : inTime;
+time = Math.trunc(time);
 let sec, min, day, hour;
 
 // Count down functionality
 setInterval(() => {
+  if (time === undefined || time < 0) {
+    // If countdown reaches zero, update launch time to the next 15 days
+    const date = new Date();
+    date.setDate(date.getDate() + 15);
+    localStorage.date = date;
+    time = (+new Date(localStorage.date) - new Date().getTime()) / 1000;
+    time = Math.trunc(time);
+  }
+
+  // Declaring to check for differences
   const newMin = min;
   const newDay = day;
   const newHour = hour;
 
+  // Set sec,min, hour, and Day units
   sec = String(time % 60).padStart(2, 0);
   min = String(Math.trunc(time / 60) % 60).padStart(2, 0);
   hour = String(Math.trunc(time / (60 * 60)) % 24).padStart(2, 0);
   day = String(Math.trunc(time / (60 * 60 * 24))).padStart(2, 0);
 
-  // animation
-  if (newMin !== min && newMin !== undefined) {
+  // To show all the count unit on load
+  if (!newHour) {
+    animate("min", min);
+    animate("day", day);
+    animate("hour", hour);
+  }
+
+  // Animate if there is a differnce
+  if (newMin !== min && newMin) {
     animate("min", min);
   }
-  if (newDay !== day && newDay !== undefined) {
+  if (newDay !== day && newDay) {
     animate("day", day);
   }
-  if (newHour !== hour && newHour !== undefined) {
+  if (newHour !== hour && newHour) {
     animate("hour", hour);
   }
   animate("sec", sec);
-  // console.log(`${day}: ${hour}: ${min}: ${sec}`);
 
   time--;
 }, 1000);
 
-// Animation function
+// Animation functionality
 function animate(unit, value) {
   document.querySelector(
     `.time__count--${unit} .count-inner--f1`
